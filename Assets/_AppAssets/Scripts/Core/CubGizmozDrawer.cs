6e579263b6e0a7b390ba4ec200d7b0c04e11bf18
Omar <Omar.h.shafeek@gmail.com>
2020-09-25 17:43:18 +0200

    Omar - Structure Implementation V1

---
+++ Assets/_AppAssets/Scripts/Core/CubGizmozDrawer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubGizmozDrawer : MonoBehaviour
+{
+    public Collider collider;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if(collider)
+        Gizmos.DrawSphere(collider.bounds.center, 0.2f);
+        Gizmos.DrawSphere(collider.bounds.center+Vector3.forward, 0.2f);
+
+        Gizmos.DrawSphere(collider.bounds.center + transform.forward, 0.2f);
+
+        Debug.Log(Vector3.Dot((collider.bounds.center - transform.forward).normalized, (collider.bounds.center - Vector3.forward).normalized));
+    }
+}
