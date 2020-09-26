23a98df7eceb1ef83328b17dcc2f3a344c2ad5ea
Marwan <marwan_ayad@Live.com>
2020-09-26 17:04:14 +0200

    working on color paterns , add cube indecatore

---
+++ Assets/_AppAssets/Scripts/General/Color_Swap_Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color_Swap_Manager : MonoBehaviour
+{
+    public static Color_Swap_Manager inst;
+
+    // List<Gradient> Color_Paltes = new List<Gradient> ();
+
+    public List<Color_Palet> Color_Paltes = new List<Color_Palet> ();
+    void Awake ()
+    {
+        inst = this;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    [System.Serializable]
+    public class Color_Palet
+    {
+        List<Gradient> Color_Paltes = new List<Gradient> ();
+    }
+}
---
+++ Assets/_AppAssets/Scripts/General/FadeinOut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+public class FadeinOut : MonoBehaviour
+{
+    Renderer ren;
+    void Start ()
+    {
+        ren = GetComponent<Renderer> ();
+        ren.sharedMaterial.DOFade (0.4f, "_TintColor", 0.5f).SetLoops (-1, LoopType.Yoyo).From (.1f);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
