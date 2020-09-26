8203b050bc7b96cc25a4564c895a9b5c19ea76ee
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2020-09-27 00:33:44 +0200

    bendary - prepare ui for integration

---
+++ Assets/_AppAssets/Scripts/General/ImportantData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ImportantData
+{
+    public const string CurrentLevel = "CurrentLevel";
+    public const string LevelScore = "Level";
+}
