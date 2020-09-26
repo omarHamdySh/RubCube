adc00ae67aa25d8bd9c043a74a9dac98c19ab6ed
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2020-09-26 10:33:30 +0200

    bendary - make the hollow controller Color

---
+++ Assets/_AppAssets/Scripts/General/HollowController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HollowController : MonoBehaviour
+{
+    public bool isHollow;
+    [SerializeField] private Material sourceMat;
+    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private Color matColor;
+
+    private Material hollowMaterial;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (isHollow)
+        {
+            if (!mesh)
+            {
+                mesh = GetComponent<MeshRenderer>();
+            }
+
+            hollowMaterial = new Material(sourceMat);
+            mesh.material = hollowMaterial;
+            ChangeMatColor(matColor);
+        }
+    }
+
+    /// <summary>
+    /// Change the Color of material
+    /// </summary>
+    /// <param name="color"></param>
+    public void ChangeMatColor(Color color)
+    {
+        //hollowMaterial.SetColor("_OutlineColor", color);
+        hollowMaterial.color = color;
+    }
+}
