484dc7fa93348479263c04c828b827ec9f454f45
Omar <Omar.h.shafeek@gmail.com>
2020-09-28 03:57:47 +0200

    Omar - Dynamic New Block Color Fetching

--- Assets/_AppAssets/Scripts/General/FadeinOut.cs
+++ Assets/_AppAssets/Scripts/General/FadeinOut.cs
@@ -8,10 +8,16 @@ public class FadeinOut : MonoBehaviour
     public Color FadeColor;
     void Start ()
     {
-        ren = GetComponent<Renderer> ();
-        ren.sharedMaterial.color = FadeColor;
+        ren = GetComponent<Renderer>();
         ren.sharedMaterial.DOFade (0.2f, "_TintColor", 0.5f).SetLoops (-1, LoopType.Yoyo).From (.05f);
 
+    }
+
+    public void updateColor(Color color)
+    {
+        FadeColor = color;
+        ren = GetComponent<Renderer>();
+        ren.sharedMaterial.SetColor("_TintColor", FadeColor);
     }
 
 
