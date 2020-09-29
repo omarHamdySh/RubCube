ad1e46c7db6edc3f267139ba4f3cffc37dd6f87e
KLQUInfinity <31450453+KLQUInfinity@users.noreply.github.com>
2020-09-29 23:59:55 +0200

    bendary - fix tutorial postion  error

--- Assets/_AppAssets/Scripts/UI/TutorialAnimation.cs
+++ Assets/_AppAssets/Scripts/UI/TutorialAnimation.cs
@@ -13,18 +13,19 @@ public class TutorialAnimation : MonoBehaviour
     public GameObject animatedElement;
     public AnimationType animationType;
     public int direction = 1;
+    public float easeValue = 150;
     // Start is called before the first frame update
     private void Start()
     {
 
-        Vector3 position = transform.position;
+        Vector2 position = transform.localPosition;
         switch (animationType)
         {
             case AnimationType.Horizontal:
-                animatedElement.transform.DOMove(new Vector3(position.x + 150 * direction, position.y, position.z), 1f).SetEase(Ease.InFlash).SetLoops(-1, LoopType.Yoyo).From(transform.position);
+                animatedElement.transform.DOLocalMoveX(position.x + easeValue * direction, 1f).SetEase(Ease.InFlash).SetLoops(-1, LoopType.Yoyo).From(transform.localPosition);
                 break;
             case AnimationType.Vertical:
-                animatedElement.transform.DOMove(new Vector3(position.x, position.y + 150 * direction, position.z), 1f).SetEase(Ease.InFlash).SetLoops(-1, LoopType.Yoyo).From(transform.position);
+                animatedElement.transform.DOLocalMoveY(position.y + easeValue * direction, 1f).SetEase(Ease.InFlash).SetLoops(-1, LoopType.Yoyo).From(transform.localPosition);
                 break;
         }
     }
